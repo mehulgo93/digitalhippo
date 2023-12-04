9404c3722f368abd99c6c273ef3c4087552fbeff
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-12-04 17:15:12 -0500

    complete landing page

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+import { useEffect, useRef, useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
@@ -15,8 +38,21 @@ const NavItems = () => {
             setActiveIndex(i);
           }
         };
+
+        const close = () => setActiveIndex(null);
+
         const isOpen = i === activeIndex;
-        return <NavItem />;
+
+        return (
+          <NavItem
+            category={category}
+            close={close}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
       })}
     </div>
   );
