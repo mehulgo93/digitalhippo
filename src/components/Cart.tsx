45212e4a53c859d5838a736677ae91f153668155
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-12-04 17:38:23 -0500

    Adding cart functionality

---
+++ src/components/Cart.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { ShoppingCart } from "lucide-react";
+
+export const Cart = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCart
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500 "
+          aria-hidden="true"
+        />
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          0
+        </span>
+      </SheetTrigger>
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle>Cart (0)</SheetTitle>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
